ad0b59f55fbfec6435e9c0595ec0b7076dc51564
Aditya <93360691+aditya-ig10@users.noreply.github.com>
2024-03-17 19:02:13 +0530

    declaring variables

---
+++ kot2.kt
@@ -0,0 +1,10 @@
+    // Declaring variables and Values using Val and Var
+    fun main() {
+        var Name = "Aditya"   // Using 'var' to declare a variable
+        val age = 20             // Using 'val' to declare a constant variable
+    
+        // Printing the values of the variables
+        // Use $ to call the predeclared variables
+        println("Name: $Name")
+        println("Age: $age")
+    }
