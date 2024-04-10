bc84f99750ba93246488990dbe021f164c51c5bf
Aditya <93360691+aditya-ig10@users.noreply.github.com>
2024-04-10 16:59:15 +0530

    ..

---
+++ demo.kt
@@ -0,0 +1,8 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("Enter your input:")
+    val userInput = scanner.nextLine()
+    println("You entered: $userInput")
+}
